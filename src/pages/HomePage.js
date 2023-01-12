3ab4fd366d999a1a4c9c37d2672b6a4e477872d9
Harish069 <harish.1994.19@gmail.com>
2023-01-13 02:01:30 +0530

    other commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,24 @@
 import { Grid, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import Navbar from "./Navbar";
+import axios from "axios";
 
 const HomePage = () => {
-
   const [file, setFile] = useState();
-    function handleChange(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
-    }
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
+   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const value = axios.get("https://github.com/harish0585/New-Weather-App.git")
+    setData(value);
+    console.log(value, "harish");
+  }, [])
+
 
   return (
     <div>
@@ -20,16 +29,8 @@ const HomePage = () => {
           style={{ padding: "100px", textAlign: "center", width: "100%" }}
         >
           <Typography variant="h3">
-            {`Welcome To Marriage Portal ${localStorage.getItem("name")}`}
+            {`Welcome  ${localStorage.getItem("name")}`}
           </Typography>
-          <h2>Upload your Image
-          <input type="file" placeholder="Upload Photo" onChange={handleChange} />
-          </h2>
-            <img style={{ height: "150px", width: "150px", border: "none" }} src={file} />
-            <br />
-            <h2>Upload your Documents
-          <input type="file" placeholder="Upload Photo" onChange={handleChange} />
-          </h2>
         </Grid>
       </Grid>
     </div>
